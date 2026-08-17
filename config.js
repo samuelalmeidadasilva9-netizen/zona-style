1485be2a4dfd893e65e31814a67788aa0d68982a
samuelalmeidadasilva9-netizen <samuelalmeidadasilva9@gmail.com>
2026-08-17 20:29:48 -0300

    Update Mercado Pago public key in config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // A Public Key do Mercado Pago pode ficar no frontend.
 // Troque o texto abaixo pela sua PUBLIC KEY de teste/produção.
 // NUNCA coloque o Access Token neste arquivo.
-window.ZONA_STYLE_MP_PUBLIC_KEY = "SUA_PUBLIC_KEY_AQUI";
+window.ZONA_STYLE_MP_PUBLIC_KEY = "TEST-29c9f5b2-4b75-4aa6-b46b-dc15332f8d6d";
